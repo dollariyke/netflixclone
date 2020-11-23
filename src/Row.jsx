7a2d4386f2193e5642b291c6c85f1dbb59a577e5
Ikemba Nwawom <nwawomikemba@gmail.com>
2020-11-23 12:06:21 +0400

    Update Row.jsx

--- src/Row.jsx
+++ src/Row.jsx
@@ -4,7 +4,7 @@ import axios from "./axios";
 import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   // A snippet of code which runs based on a specific condition/variable.
   // We want to be able to pull information from an API so we will use UseEffect to retrieve information and feed the rows.
@@ -36,8 +36,10 @@ function Row({ title, fetchUrl }) {
           return (
             <img
               key={movie.id}
-              className="row_poster"
-              src={`${base_url}${movie.poster_path}`}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
               alt={movie.name}
             />
           );
