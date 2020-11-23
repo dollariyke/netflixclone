4bbe82aac3fba058ad4385f44a80c1e6857917a9
Ikemba Nwawom <nwawomikemba@gmail.com>
2020-11-23 12:05:52 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ import requests from "./request";
 function App() {
   return (
     <div className="App">
-      <h1> Hey! Ikemba let's build Netflix clone frontend</h1>
+      {/**Navbar */}
+      {/**Banner */}
       {/**Each row is going to have a different request from others as you can see */}
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginal} />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginal}
+        isLargeRow
+      />
       <Row title="Netflix Trending" fetchUrl={requests.fetchTrending} />
       <Row title="Top rated" fetchUrl={requests.fetchTopRated} />
     </div>
