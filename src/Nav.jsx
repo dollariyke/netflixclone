e29b1a30f43d362052ed7364f0d5e62d8d5a3245
Ikemba Nwawom <nwawomikemba@gmail.com>
2020-11-24 18:09:35 +0400

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import "./Nav.css";
 
 function Nav() {
   return (
     <div className="nav">
       <img
+        className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
-        alt=""
+        alt="Netflix Logo"
       />
-      <img src="https://pbs.twimg.com/media/DlKNEufWsAAgr2E.jpg" alt="" />
+      <img
+        className="nav_avatar"
+        src="https://pbs.twimg.com/media/DlKNEufWsAAgr2E.jpg"
+        alt="Netflix Logo"
+      />
     </div>
   );
 }
