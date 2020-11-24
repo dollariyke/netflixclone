ae39a33b64cb23363d564b09fb07da6137e41616
Ikemba Nwawom <nwawomikemba@gmail.com>
2020-11-24 17:52:36 +0400

    Create Nav.jsx

---
+++ src/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
+        alt=""
+      />
+      <img src="https://pbs.twimg.com/media/DlKNEufWsAAgr2E.jpg" alt="" />
+    </div>
+  );
+}
+
+export default Nav;
