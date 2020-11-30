74689a732e90f5359b3f3af4db8168bd0b61d6dd
Ikemba Nwawom <nwawomikemba@gmail.com>
2020-11-30 06:54:39 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import Banner from "./Banner";
-import Nav from "./Nav.jsx";
+import NavBar from "./NavBar.jsx";
 
 function App() {
   return (
     <div className="App">
       {/**Navbar */}
-      <Nav />
+      <NavBar />
       {/**Banner */}
       <Banner />
       {/**Each row is going to have a different request from others as you can see */}
       <Row
         title="Harrypotter"
         fetchUrl={"http://www.omdbapi.com/?apikey=fef030f5&s=harry%20potter"}
-        isLargeRow
       />
       <Row
         title="Batman"
