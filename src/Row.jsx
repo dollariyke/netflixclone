9e4f0e290956928e23374a353c6cf88b7f1eb5f7
Ikemba Nwawom <nwawomikemba@gmail.com>
2020-11-30 06:54:54 +0400

    Update Row.jsx

--- src/Row.jsx
+++ src/Row.jsx
@@ -6,22 +6,19 @@ import "./Row.css";
 class Row extends React.Component {
   state = {
     movieDetails: [],
-    loading: true,
+    loading: false,
   };
   componentDidMount = async () => {
+    this.setState({ loading: true });
     this.fetchMovie();
   };
 
   fetchMovie = async () => {
-    this.setState({ loading: true });
     try {
       let response = await fetch(this.props.fetchUrl);
       if (response.ok) {
         let results = await response.json();
-
-        setTimeout(() => {
-          this.setState({ movieDetails: results.Search, loading: false });
-        }, 1000);
+        this.setState({ movieDetails: results.Search, loading: false });
       } else {
         console.log("an error happened!");
         this.setState({ loading: false });
